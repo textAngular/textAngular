71940fbb56633e625c3cdbc4c3d31398462047d6
Ting-Kuan <gintau2000@gmail.com>
2014-02-03 23:11:13 -0500

    Add a directive handling shortcuts.

---
+++ TextAngularCtrl.js
@@ -0,0 +1,6 @@
+﻿var app = angular.module('textAngularTestingApp', ['textAngular']);
+
+app.controller('textAngularTestingCtrl', ['$scope', function ($scope) {
+    $scope.ctrlName = 'textAngularTestingCtrl';
+    $scope.editorContent = 'original text in editor';
+}]);
