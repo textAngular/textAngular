cca85d72d90e858ba9cfa4bafcad6e13695acb81
Ting-Kuan <gintau2000@gmail.com>
2014-02-04 00:39:00 -0500

    Revert "Add a directive handling shortcuts."
    This reverts commit 71940fbb56633e625c3cdbc4c3d31398462047d6.

--- TextAngularCtrl.js
+++
@@ -1,6 +0,0 @@
-﻿var app = angular.module('textAngularTestingApp', ['textAngular']);
-
-app.controller('textAngularTestingCtrl', ['$scope', function ($scope) {
-    $scope.ctrlName = 'textAngularTestingCtrl';
-    $scope.editorContent = 'original text in editor';
-}]);
