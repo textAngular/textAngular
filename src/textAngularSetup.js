80da7e9713851191728f3cc93422cb5ab0f1ad41
Keith <keith@keithfimreite.com>
2014-05-28 17:35:22 -0700

    Update textAngularSetup.js

--- src/textAngularSetup.js
+++ src/textAngularSetup.js
@@ -13,7 +13,7 @@ textAngularSetup.value('taOptions',  {
 	toolbar: [
 		['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'pre', 'quote'],
 		['bold', 'italics', 'underline', 'ul', 'ol', 'redo', 'undo', 'clear'],
-		['justifyLeft', 'justifyCenter', 'justifyRight', 'indent', 'outdent'],
+		['justifyLeft','justifyCenter','justifyRight','indent','outdent'],
 		['html', 'insertImage', 'insertLink', 'insertVideo']
 	],
 	classes: {
