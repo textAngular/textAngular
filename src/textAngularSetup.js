3913b31ab053cfeefa301a095515ce01292b0f43
SimeonC <Simeon@lume.co.nz>
2014-10-03 15:52:44 +1300

    fix(formatting) Fix tabbing

--- src/textAngularSetup.js
+++ src/textAngularSetup.js
@@ -158,7 +158,7 @@ angular.module('textAngularSetup', [])
 	wordcount: {
 		tooltip: 'Display words Count'
 	},
-        charcount: {
+		charcount: {
 		tooltip: 'Display characters Count'
 	}
 })
@@ -620,45 +620,45 @@ angular.module('textAngularSetup', [])
 	taRegisterTool('wordcount', {
 		display: '<div id="toolbarWC" style="display:block; width:100px;">Words:{{wordcount}}</div>',
 		activeState: function(){ // this fires on keyup
-            if( this.$editor().displayElements)
-            {
-                var textElement = this.$editor().displayElements.text;
-                var sourceText = textElement[0].innerText || textElement[0].textContent; // to cover the non-jquery use case.
+			if( this.$editor().displayElements)
+			{
+				var textElement = this.$editor().displayElements.text;
+				var sourceText = textElement[0].innerText || textElement[0].textContent; // to cover the non-jquery use case.
 
-                // Caculate number of words
-                var sourceTextArray = sourceText.replace(/\s+/g,' ').split(' ');
-                var noOfWords = 0;
-                for (var i = 0; i < sourceTextArray.length; i++) {
-                    if (sourceTextArray[i] !== '') {
-                        noOfWords++;
-                    }
-                }
+				// Caculate number of words
+				var sourceTextArray = sourceText.replace(/\s+/g,' ').split(' ');
+				var noOfWords = 0;
+				for (var i = 0; i < sourceTextArray.length; i++) {
+					if (sourceTextArray[i] !== '') {
+						noOfWords++;
+					}
+				}
 
-                //Set current scope
-                this.wordcount = noOfWords;
-                //Set editor scope
-                this.$editor().wordcount = noOfWords;
-            }
-            return false;
-        }
+				//Set current scope
+				this.wordcount = noOfWords;
+				//Set editor scope
+				this.$editor().wordcount = noOfWords;
+			}
+			return false;
+		}
 	});
 	taRegisterTool('charcount', {
-            display: '<div id="toolbarCC" style="display:block; width:120px;">Characters:{{charcount}}</div>',
-            activeState: function(){ // this fires on keyup
-            if( this.$editor().displayElements)
-            {
-                var textElement = this.$editor().displayElements.text;
-                var sourceText = textElement[0].innerText || textElement[0].textContent; // to cover the non-jquery use case.
+			display: '<div id="toolbarCC" style="display:block; width:120px;">Characters:{{charcount}}</div>',
+			activeState: function(){ // this fires on keyup
+			if( this.$editor().displayElements)
+			{
+				var textElement = this.$editor().displayElements.text;
+				var sourceText = textElement[0].innerText || textElement[0].textContent; // to cover the non-jquery use case.
 
-                // Caculate number of chars
-                var noOfChars = sourceText.replace(/^\s+/g,' ').replace(/\s+$/g, ' ').length;
+				// Caculate number of chars
+				var noOfChars = sourceText.replace(/^\s+/g,' ').replace(/\s+$/g, ' ').length;
 
-                //Set current scope
-                this.charcount = noOfChars;
-                //Set editor scope
-                this.$editor().charcount = noOfChars;
-            }
-            return false;
-        }
-    });
+				//Set current scope
+				this.charcount = noOfChars;
+				//Set editor scope
+				this.$editor().charcount = noOfChars;
+			}
+			return false;
+		}
+	});
 }]);
