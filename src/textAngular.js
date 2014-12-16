b4aa7e8e752436e9b0e43db77d512d1d90fe626f
SimeonC <Simeon@lume.co.nz>
2014-12-17 10:52:05 +1300

    chore(release): Build Dist files

--- src/textAngular.js
+++ src/textAngular.js
@@ -2,7 +2,7 @@
 @license textAngular
 Author : Austin Anderson
 License : 2013 MIT
-Version 1.3.0-17
+Version 1.3.0-18
 
 See README.md or https://github.com/fraywing/textAngular/wiki for requirements and use.
 */
