f6d8e914836c5ecda87816db7e6c3f088a30eade
Robin Andersson <me@robinwassen.com>
2015-01-20 10:43:35 +0100

    Cleaned up the demo

---
+++ js/demo.js
@@ -0,0 +1,9 @@
+(function() {
+	angular
+		.module("TextAngularDemo", ['textAngular'])
+		.controller("DemoController", DemoController);
+
+	function DemoController($scope) {
+		$scope.htmlContent = '<h2>Try me!</h2><p>textAngular is a super cool WYSIWYG Text Editor directive for AngularJS</p><p><b>Features:</b></p><ol><li>Automatic Seamless Two-Way-Binding</li><li style="color: blue;">Super Easy <b>Theming</b> Options</li><li>Simple Editor Instance Creation</li><li>Safely Parses Html for Custom Toolbar Icons</li><li>Doesn&apos;t Use an iFrame</li><li>Works with Firefox, Chrome, and IE8+</li></ol><p><b>Code at GitHub:</b> <a href="https://github.com/fraywing/textAngular">Here</a> </p>';
+	};
+})();
