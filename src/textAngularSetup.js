49b7df2d1513b4665e80e17536d465e35f15f7da
Ge Yang <yangge1987@gmail.com>
2015-06-18 01:38:10 -0500

    handle corner case when commonElement is document.
    angular returns an error when the cursor is not inside the editor. This happens when the `commonElement === <'#document'>`. 
    
    this case should be escaped properly.

--- src/textAngularSetup.js
+++ src/textAngularSetup.js
@@ -277,6 +277,7 @@ angular.module('textAngularSetup', [])
 		},
 		activeState: function(commonElement){
 			var result = false;
+			if (!commonElement || commonElement.nodeName === '#document') return false;
 			if(commonElement) result =
 				commonElement.css('text-align') === 'left' ||
 				commonElement.attr('align') === 'left' ||
@@ -299,6 +300,7 @@ angular.module('textAngularSetup', [])
 		},
 		activeState: function(commonElement){
 			var result = false;
+			if (!commonElement || commonElement.nodeName === '#document') return false;
 			if(commonElement) result = commonElement.css('text-align') === 'right';
 			result = result || this.$editor().queryCommandState('justifyRight');
 			return result;
@@ -312,6 +314,7 @@ angular.module('textAngularSetup', [])
 		},
 		activeState: function(commonElement){
 			var result = false;
+			if (!commonElement || commonElement.nodeName === '#document') return false;
 			if(commonElement) result = commonElement.css('text-align') === 'center';
 			result = result || this.$editor().queryCommandState('justifyCenter');
 			return result;
