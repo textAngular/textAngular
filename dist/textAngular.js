bb7b884f447e8889b0dc75e4f74767fca702e585
Joel Parke <joel@Dynazu.com>
2015-07-11 00:57:21 -0600

    chore(release): Build Dist files

--- dist/textAngular.js
+++ dist/textAngular.js
@@ -2,7 +2,7 @@
 @license textAngular
 Author : Austin Anderson
 License : 2013 MIT
-Version 1.4.1
+Version 1.4.2
 
 See README.md or https://github.com/fraywing/textAngular/wiki for requirements and use.
 */
