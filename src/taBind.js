2e33b9e560f5268aa32e9711727049d97b259464
Yalda Kako <ykako@pavlovmedia.com>
2015-09-03 13:37:52 -0500

    fix 644: parse whitespace from plaintexT

--- src/taBind.js
+++ src/taBind.js
@@ -534,6 +534,15 @@ angular.module('textAngular.taBind', ['textAngular.factories', 'textAngular.DOM'
 								// insert missing parent of li element
 								text = text.replace(/<li(\s.*)?>.*<\/li(\s.*)?>/i, '<ul>$&</ul>');
 							}
+														
+							// parse whitespace from plaintext input, starting with preceding spaces that get stripped on paste
+							text = text.replace(/^[ |\u00A0]+/gm, function (match) {
+								var result = '';
+								for (var i = 0; i < match.length; i++) {
+																	result += '&nbsp;';
+								}
+								return result;
+							}).replace(/\n|\r\n|\r/g, '<br />').replace(/\t/g, '&nbsp;&nbsp;&nbsp;&nbsp;');
 
 							if(_pasteHandler) text = _pasteHandler(scope, {$html: text}) || text;
 
