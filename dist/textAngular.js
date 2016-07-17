60e1cdfbe6e5e234f41d915b83fba105b466fab4
David Kettler <21echoes@gmail.com>
2016-07-17 00:38:05 -0700

    pull rich text into just one file

--- dist/textAngular.js
+++ dist/textAngular.js
@@ -2,7 +2,7 @@
 @license textAngular
 Author : Austin Anderson
 License : 2013 MIT
-Version 1.5.2-patreon
+Version 1.5.2-patreon-rc1
 
 See README.md or https://github.com/fraywing/textAngular/wiki for requirements and use.
 */
@@ -415,6 +415,102 @@ angular.module('textAngular.factories', [])
 	};
 }]);
 
+angular.module('PatreonRichText', [])
+	.factory('PatreonRichText', function($timeout, $document, $window) {
+		'ngInject';
+
+		/*eslint-disable */
+
+		function placeTooltipAtCoordinates(x, y, callback) {
+			var _offsetX = 24,
+			_offsetY = 60;
+
+			return $timeout(function(){
+				Array.prototype.forEach.call(
+					document.getElementsByClassName('ta-toolbar'),
+					function(node) {
+						node.style.left = String(x - _offsetX) + 'px';
+						node.style.top = String(y - _offsetY) + 'px';
+					}
+				);
+
+				if (callback) {
+					return callback();
+				}
+			});
+		}
+
+		function getTextRectangles(selection){
+			var range = selection.getRangeAt(0),
+			rectangles = null;
+
+			rectangles = range.getClientRects();
+
+			return rectangles;
+		}
+
+		var PatreonRichText = {};
+
+		PatreonRichText.customTextAreaElement = "<textarea class='post-creation-textarea' id='post-creation-textarea'></textarea>";
+
+		PatreonRichText.customToolbarElement = '<div class="ta-link-toolbar"></div>';
+
+		PatreonRichText.setUpIcon = function(iconClass){
+			return angular.element("<i data-patreon-icon='" + iconClass + "'>");
+		};
+
+		PatreonRichText.showPopover = function(scope){
+			scope.displayElements.popover.addClass('active');
+		};
+
+		PatreonRichText.hidePopover = function(scope){
+			scope.displayElements.popover.removeClass('active');
+		};
+
+		PatreonRichText.hideToolbarPopover = function(){
+			return placeTooltipAtCoordinates(-9999, -9999);
+		};
+
+		PatreonRichText.handleSelected = function(scope, focussedCallback){
+			var selection = $window.getSelection();
+			var currentlySelected = selection.type ? selection.type === "Range" : !selection.isCollapsed; // FF compatibility
+			var popoverIsActive = scope.displayElements.popover.hasClass('active');
+			var popoverInputIsActive = Array.prototype.some.call(
+				scope.displayElements.popover[0].querySelectorAll('.patreon-input'),
+				function(node) { return node === $document.activeElement; }
+			);
+			if (currentlySelected && !popoverIsActive) {
+				var rectangles = getTextRectangles($window.getSelection());
+				if (rectangles[0] && rectangles[0].left && rectangles[0].top) {
+					focussedCallback(true);
+
+					return placeTooltipAtCoordinates( rectangles[0].left, rectangles[0].top);
+				} else {
+					return this.hideToolbarPopover();
+				}
+			} else if (!currentlySelected && popoverIsActive && !popoverInputIsActive) {
+				return this.hideToolbarPopover();
+			} else {
+				return this.hideToolbarPopover();
+			}
+
+			return false;
+		};
+
+		PatreonRichText.handleSelectedOnMouseUp = function(){
+			$timeout(this.handleSelected);
+		};
+
+		PatreonRichText.scrollEventForRemovingTooltips = function(scope){
+			PatreonRichText.hideToolbarPopover(scope);
+
+			return $timeout(angular.noop);
+		};
+
+		/*eslint-enable */
+
+		return PatreonRichText;
+	});
 
 angular.module('textAngular.DOM', ['textAngular.factories'])
 .factory('taExecCommand', ['taSelection', 'taBrowserTag', '$document', function(taSelection, taBrowserTag, $document){
--- src/patreon_rich_text/index.js
+++
@@ -1,7 +0,0 @@
-import angular from 'angular';
-import PatreonRichText from './patreon-rich-text';
-
-export const angularModuleName = 'PatreonRichText';
-
-angular.module(angularModuleName, [])
-    .factory('PatreonRichText', PatreonRichText);
