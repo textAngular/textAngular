ba44ad5d71cc09f98f244b679296493b4627bd40
Joel Parke <joel@Dynazu.com>
2016-08-17 13:52:16 -0600

    Fix(js/demo): Use the version from textAngularMananger

--- js/demo.js
+++ js/demo.js
@@ -1,9 +1,10 @@
 (function() {
 	angular
 		.module("TextAngularDemo", ['textAngular'])
-		.controller("DemoController", DemoController);
+		.controller("DemoController", ['$scope', 'textAngularManager', DemoController]);
 
-	function DemoController($scope) {
+	function DemoController($scope, textAngularManager) {
+		$scope.version = textAngularManager.getVersion();
 		$scope.htmlContent = '<h2>Try me!</h2><p>textAngular is a super cool WYSIWYG Text Editor directive for AngularJS</p><p><b>Features:</b></p><ol><li>Automatic Seamless Two-Way-Binding</li><li style="color: blue;">Super Easy <b>Theming</b> Options</li><li>Simple Editor Instance Creation</li><li>Safely Parses Html for Custom Toolbar Icons</li><li>Doesn&apos;t Use an iFrame</li><li>Works with Firefox, Chrome, and IE8+</li></ol><p><b>Code at GitHub:</b> <a href="https://github.com/fraywing/textAngular">Here</a> </p>';
 	};
 })();
