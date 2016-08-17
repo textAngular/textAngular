974a7013e348b24ac926306e03d5815f52f20a6d
Joel Parke <joel@Dynazu.com>
2016-08-16 20:54:16 -0600

    fix(main, testAngular.spec): removed log messages and added a test

--- src/main.js
+++ src/main.js
@@ -1120,11 +1120,9 @@ textAngular.directive('textAngularToolbar', [
 textAngular.directive('textAngularVersion', ['textAngularManager',
 	function(textAngularManager) {
 		var version = textAngularManager.getVersion();
-		console.log(version);
 		return {
 			restrict: "EA",
 			link: function (scope, element, attrs) {
-				console.log(element);
 				element.html(version);
 			}
 		};
