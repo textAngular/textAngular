5473d019ce1bf3d1fb7140ad7a1e2b01e428a84e
Joel Parke <joel@Dynazu.com>
2016-09-16 16:18:13 -0600

    fix(taBind): Browser spelling correction not setting the control to dirty #1120 fix(textAngularSetup): TypeError: Cannot read property 'length' of null #987

--- src/taBind.js
+++ src/taBind.js
@@ -900,6 +900,31 @@ angular.module('textAngular.taBind', ['textAngular.factories', 'textAngular.DOM'
 						}
 					});
 
+					// when there is a change from a spelling correction in the browser, the only
+					// change that is seen is a 'input' and the $watch('html') sees nothing... So
+					// we added this element.on('input') to catch this change and call the _setViewValue()
+					// so the ngModel is updated and all works as it should.
+					var _inputTimeout;
+					element.on('input', function() {
+						if (_compileHtml() !== ngModel.$viewValue) {
+							// we wait a time now to allow the natural $watch('html') to handle this change
+							// and then after a 1 second delay, if there is still a difference we will do the
+							// _setViewValue() call.
+							/* istanbul ignore if: can't test */
+							if(_inputTimeout) $timeout.cancel(_inputTimeout);
+							/* istanbul ignore next: cant' test? */
+							_inputTimeout = $timeout(function() {
+								var _val = _compileHtml();
+								if (_val !== ngModel.$viewValue) {
+									//console.log('_setViewValue');
+								    //console.log('old:', ngModel.$viewValue);
+									//console.log('new:', _val);
+									_setViewValue(_val, true);
+								}
+							}, 1000);
+						}
+					});
+
 					element.on('blur', scope.events.blur = function(){
 						_focussed = false;
 						/* istanbul ignore else: if readonly don't update model */
