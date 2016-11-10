4845f20a30817541c4d6affe7f85dec63dc260cc
Ruslan Bekenev <furyinbox@gmail.com>
2016-11-10 20:46:27 +0300

    fixed issue #1201 with <img> tag when it inserts outside of <p>

--- src/globals.js
+++ src/globals.js
@@ -75,7 +75,7 @@ function getDomFromHtml(html)
 
 // Global to textAngular REGEXP vars for block and list elements.
 
-var BLOCKELEMENTS = /^(address|article|aside|audio|blockquote|canvas|center|dd|div|dl|fieldset|figcaption|figure|footer|form|h1|h2|h3|h4|h5|h6|header|hgroup|hr|noscript|ol|output|p|pre|section|table|tfoot|ul|video)$/i;
+var BLOCKELEMENTS = /^(address|article|aside|audio|blockquote|canvas|center|dd|div|dl|fieldset|figcaption|figure|footer|form|h1|h2|h3|h4|h5|h6|header|hgroup|hr|noscript|ol|output|p|pre|section|table|tfoot|ul|video|img)$/i;
 var LISTELEMENTS = /^(ul|li|ol)$/i;
 // updated VALIDELEMENTS to include #text and span so that we can use nodeName instead of tagName
 var VALIDELEMENTS = /^(#text|span|address|article|aside|audio|blockquote|canvas|center|dd|div|dl|fieldset|figcaption|figure|footer|form|h1|h2|h3|h4|h5|h6|header|hgroup|hr|noscript|ol|output|p|pre|section|table|tfoot|ul|video|li)$/i;
