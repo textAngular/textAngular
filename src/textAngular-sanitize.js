60e802a819c922f217d8f8830ac5df4af323f68d
Glenn Fowler <glenn@cebjyre.com>
2017-01-23 17:24:20 +1100

    Fix compilation error
    I dropped statements in the middle of a set of conditionals; rewritten to fix it.

--- src/textAngular-sanitize.js
+++ src/textAngular-sanitize.js
@@ -513,6 +513,12 @@ var trim = (function() {
 function validStyles(styleAttr){
 	var result = '';
 	var styleArray = styleAttr.split(';');
+	
+	//This regex is a bit of a monster, so composing it here to try to make it a little less scary
+	var lengthComponent = '([0-9\.]+(px|em|rem))?';
+	var styleComponent = '(none|hidden|dotted|dashed|solid|double|groove|ridge|inset|outset)?';
+	var colorComponent = '((rgb|hsl)a?\([0-9%,\.]+\)|#[0-9a-f]{3,6}|[a-z]+)?';
+	var borderRegex = new RegExp('^'+lengthComponent+'\s*'+styleComponent+'\s*'+colorComponent+'$', 'i');
 	angular.forEach(styleArray, function(value){
 		var v = value.split(':');
 		if(v.length == 2){
@@ -607,11 +613,6 @@ function validStyles(styleAttr){
 				))
 			||
 				((key === 'border' || key === 'border-bottom' || key === 'border-top' || key === 'border-right' || key === 'border-left') && (
-					var lengthComponent = '([0-9\.]+(px|em|rem))?';
-					var styleComponent = '(none|hidden|dotted|dashed|solid|double|groove|ridge|inset|outset)?';
-					var colorComponent = '((rgb|hsl)a?\([0-9%,\.]+\)|#[0-9a-f]{3,6}|[a-z]+)?';
-					var borderRegex = new RegExp('^'+lengthComponent+'\s*'+styleComponent+'\s*'+colorComponent+'$', 'i');
-					
 					value.match(borderRegex)
 				))
 			) result += key + ': ' + value + ';';
