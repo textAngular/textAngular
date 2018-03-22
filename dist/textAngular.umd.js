8e2ccaa716f7ee94d9a75382122ce81cdfe613d9
alberto.maghini <alberto.maghini@deltatre.com>
2018-03-22 14:16:49 +0100

    PR for issue #1583. Reviewed selection method

--- dist/textAngular.umd.js
+++ dist/textAngular.umd.js
@@ -4,13 +4,13 @@
     define('textAngular', ["rangy","rangy/lib/rangy-selectionsaverestore"], function (a0,b1) {
       return (root['textAngular.name'] = factory(a0,b1));
     });
-  } else if (typeof exports === 'object') {
+  } else if (typeof module === 'object' && module.exports) {
     // Node. Does not work with strict CommonJS, but
     // only CommonJS-like environments that support module.exports,
     // like Node.
     module.exports = factory(require("rangy"),require("rangy/lib/rangy-selectionsaverestore"));
   } else {
-    root['textAngular'] = factory(rangy);
+    root['textAngular'] = factory(root["rangy"]);
   }
 }(this, function (rangy) {
 
